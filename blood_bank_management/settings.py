7c16bde506c136ed751e422b07592359be0b486e
Naimur Rahman <naimur.naiyimu@gmail.com>
2025-07-10 20:35:43 +0600

    [Update] SSL

--- blood_bank_management/settings.py
+++ blood_bank_management/settings.py
@@ -75,7 +75,8 @@ WSGI_APPLICATION = 'blood_bank_management.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
